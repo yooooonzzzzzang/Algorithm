dc753503ad6602ebf1cf2e5a978b3ed222e37fd5
yooooonzzzzzang <cherishvalue@naver.com>
2022-08-09 22:58:03 +0900

    docs : Update 01_List1/03_View/s1.py

--- 01_List1/03_View/s1.py
+++ 01_List1/03_View/s1.py
@@ -9,8 +9,15 @@ for tc in range(1, t+1):
 
     view_good = 0   # 조망권이 좋은 세대 수
 
-    for i in building_list: # [2] 부터 시작 ^^ 집가서 다시 해보자 ^^ 화이팅~~~~~
-        if building_list[i]
+    for i in range(2, num-2):   # [2] 부터 [num-3]까지 ex) num = 100인 경우 [2] ~ [97]  인덱스는 0~99까지 있으니까)
+        max_num = 0
+        for j in range(i-2, i+3):   # [i-2] 부터 [i-2] 까지 돌리면서 최대값을 구함
+            if j == i:  # 비교할 [i] 인 경우에는 제외
+                continue
+            if building_list[j] > max_num:  # 최대값 구하기
+                max_num = building_list[j]
 
+        if max_num < building_list[i]:  # [i] 가 주변 아파트 보다 높아야하기때문에
+            view_good += (building_list[i] - max_num)   # [i] 와 max 의 차이가 뷰가 좋은 곳
 
     print(f'#{tc} {view_good}')
