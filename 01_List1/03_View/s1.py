746905011eaa91647fda66549b48062310718000
yooooonzzzzzang <cherishvalue@naver.com>
2022-08-09 17:55:07 +0900

    docs: ix 03_View s1.pyy

--- 01_List1/03_View/s1.py
+++ 01_List1/03_View/s1.py
@@ -7,7 +7,10 @@ for tc in range(1, t+1):
 
     building_list = list(map(int, input().split()))
 
-    view_good = 0
+    view_good = 0   # 조망권이 좋은 세대 수
+
+    for i in building_list: # [2] 부터 시작 ^^ 집가서 다시 해보자 ^^ 화이팅~~~~~
+        if building_list[i]
 
 
-    print(f'#{tc} {building_list}')
+    print(f'#{tc} {view_good}')
